57c83589e42c8c8893e97aefdddee2651284ad14
Matt Carroll <matt@blinkmobile.com.au>
2017-03-24 10:46:56 +1100

    SC-63 # Added Analytics for logging events

---
+++ index.js
@@ -0,0 +1,14 @@
+/* @flow */
+'use strict'
+
+const Analytics = require('./lib/Analytics.js')
+
+module.exports = Analytics
+
+/* ::
+export type AnalyticsConstructorOptions = {
+  accessKey?: string,
+  secretKey?: string,
+  origin?: string | void
+}
+*/
