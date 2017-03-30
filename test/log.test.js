16f2af7efb1f07f594acb9f2e53b9accb23c4596
Kynan Hughes <kynan@blinkmobile.com.au>
2017-03-30 16:33:36 +1100

    This is an integration test designed to run against the Dockerised instance of simple-analytics + it's database

---
+++ test/log.test.js
@@ -0,0 +1,43 @@
+/* @flow */
+'use strict'
+
+const jwt = require('jsonwebtoken')
+
+// Matches the key in the db
+// SEE:
+// ./service/simple-analytics/scripts/mongo-setup.js
+// ./service/simple-analytics/lib/models/Key.js
+// ./service/simple-analytics/lib/models/Organisation.js
+const ACCESS_KEY = '58dc86987e2c7853a6cdf008'
+const SECRET_KEY = 'SECRET_KEY'
+
+const eventToLog = [{
+  'name': 'TEST_EVENT',
+  '__v': 0
+}]
+
+const secret = SECRET_KEY
+// NOTE: This "iss" will change when the container is rebuilt
+const token = jwt.sign(
+  {
+    iss: ACCESS_KEY,
+    exp: Date.now() + 10000
+  },
+  secret
+)
+
+describe('log()', () => {
+  test('it should return a 200 response', () => {
+    const events = require('../lib/events.js')
+    expect.assertions(1)
+    return events.log(
+      'http://localhost:3000',
+      token,
+      'TEST_EVENT',
+      eventToLog
+    )
+    .then((res) => {
+      return expect(res.events[0].name).toBe('TEST_EVENT')
+    })
+  })
+})
